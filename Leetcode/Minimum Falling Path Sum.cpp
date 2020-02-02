1680078ae399b85cbce41173b0e6fc97dda4cded
amana632 <be10674.17@bitmesra.ac.in>
2020-02-02 14:47:50 +0530

    Solved

--- Leetcode/Minimum Falling Path Sum.cpp
+++ Leetcode/Minimum Falling Path Sum.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
+     int mini=INT_MAX;
+    int minsum(vector<vector<int>>& a, int sx,int sy,int dx, int dy){
+                cout<<sx<<" "<<sy<<endl;
+        
+
+        if(sx>=dx||sy>=dy||sx<0||sy<0) return INT_MAX;
+        cout<<a[sx][sy]<<endl;
+        for(int i=sy;i<dy;i++){
+            if(sx+1<dx)
+            {int ans = a[sx][i] + 
+                min( minsum(a,sx+1,i+1,dx,dy) , 
+                    min( minsum(a,sx+1,i,dx,dy) ,
+                        minsum(a,sx+1,i-1,dx,dy)));
+            if(ans<mini) mini=ans;
+            cout<<ans<<endl;}
+        }
+      return mini;
+    }
     int minFallingPathSum(vector<vector<int>>& A) {
-     int dx=A[0].size();
-     int dy=A.size();
+     int dy=A[0].size();
+     int dx=A.size();
         return minsum(A,0,0,dx,dy);
         
     }
-    int min=INT_MAX;
-    int minsum(vector<vector<int>>& a, int sx,int sy,int dx, int dy){
-        cout<<sx<<" "<<sy<<endl;
-        if(sx>=dx||sy>=dy) return INT_MAX;
-        for(int i=sx;i<dx;i++){
-            int ans = a[sx][sy] + 
-                min( minsum(a,sx+i,sy,dx,dy) , 
-                    min( minsum(a,sx+i,sy-1,dx,dy) ,
-                        minsum(a,sx+i,sy+1,dx,dy)));
-            if(ans<min) min=ans;
-        }
-      return min;
-    }
+   
 };
