1fb89c74f1f1217d6d2bed7e0cb5e84eb9830d58
amana632 <be10674.17@bitmesra.ac.in>
2020-03-18 13:11:02 +0530

    Day2

---
+++ gfg/number of open doors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#define ll long long int
+using namespace std;
+
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<floor(sqrt(n))<<endl;
+    }
+	//code
+	return 0;
+}
